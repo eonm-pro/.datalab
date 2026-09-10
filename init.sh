c305fecf9f9d0570e86f1e5a89637b04512defaa
Mathis EON <60341438+eonm-pro@users.noreply.github.com>
2026-09-10 09:27:40 +0200

    fix(init): export nix cmds to .profile .bashrc

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
+
+export PATH="$HOME/.nix-profile/bin:$PATH"
 
 # Install nix
 # Enable flakes + the new nix CLI
@@ -10,9 +13,12 @@ echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 # Provides nix-shell
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl -y
+
 sh <(curl -L https://nixos.org/nix/install) --no-daemon --yes
 . ~/.nix-profile/etc/profile.d/nix.sh
 
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
 nix-channel --update
 
+echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.profile
