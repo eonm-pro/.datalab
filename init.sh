7a2a339681b18ef73465d6b186743a2381dd1830
Mathis EON <60341438+eonm-pro@users.noreply.github.com>
2026-09-10 15:02:27 +0200

    test

--- init.sh
+++ init.sh
@@ -23,4 +23,5 @@ nix-channel --update
 echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.bashrc
 echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.profile
 
-nix registry add datalab path:/"$WORKSPACE_DIR/$(basename ${GIT_REPOSITORY:-datalab})"
+nix registry add datalab2 /home/onyxia/work/.datalab
+nix registry add datalab "$WORKSPACE_DIR/$(basename ${GIT_REPOSITORY:-datalab})"
