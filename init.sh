0e302158fc0d1a27b7f686e7254bd7889ba32182
Mathis EON <60341438+eonm-pro@users.noreply.github.com>
2026-09-10 14:51:46 +0200

    fix(init): change nix registry add command to use path syntax

--- init.sh
+++ init.sh
@@ -23,4 +23,4 @@ nix-channel --update
 echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.bashrc
 echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.profile
 
-nix registry add datalab "$WORKSPACE_DIR/$(basename ${GIT_REPOSITORY:-datalab})"'
+nix registry add datalab path:/"$WORKSPACE_DIR/$(basename ${GIT_REPOSITORY:-datalab})"
